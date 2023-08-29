e770d2cb77b379167be1a9ba7306b02c52355d69
LubYT <lubister.vip@mail.ru>
2023-08-29 15:50:38 +0300

    2

--- Plugins/main.py
+++ Plugins/main.py
@@ -1,7 +1,8 @@
 import pygame,sys
 pygame.init()
 
-window = pygame.display.set_mode((1960,1080))
+window = pygame.display.set_mode((pygame.display.Info().current_w,pygame.display.Info().current_h))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
