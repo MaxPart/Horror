53bfc64708228d01bbf452edb5c3c6aa8849416a
LubYT <lubister.vip@mail.ru>
2023-08-29 16:28:08 +0300

    3

--- Plugins/main.py
+++ Plugins/main.py
@@ -3,10 +3,18 @@ import player
 pygame.init()
 Player=player.Player()
 
-window = pygame.display.set_mode((pygame.display.Info().current_w,pygame.display.Info().current_h))
+width_base=pygame.display.Info().current_w
+height_base=pygame.display.Info().current_h
 
-r=pygame.Rect(100,100,900,900)
+window = pygame.display.set_mode((width_base,height_base))
+window.fill((105,0,158))
+
+r=pygame.Rect(100,100,int(width_base)-200,int(height_base)-200)
 pygame.draw.rect(window,(135,0,178),r,0)
+
+font = pygame.font.SysFont('Bahnschrift', 80, bold=True)
+text = font.render(str(f"MaxPart's game"), True, 'green')
+window.blit(text,(int(width_base//2-350),int(height_base//2-200)))
 
 while True:
     for event in pygame.event.get():
