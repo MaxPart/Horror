019175b52dd53bc38b4a9d78bcf12d4aec7ed709
LubYT <lubister.vip@mail.ru>
2023-08-29 16:17:02 +0300

    3

--- Plugins/main.py
+++ Plugins/main.py
@@ -5,13 +5,15 @@ Player=player.Player()
 
 window = pygame.display.set_mode((pygame.display.Info().current_w,pygame.display.Info().current_h))
 
-#ПЕНИС
+r=pygame.Rect(100,100,900,900)
+pygame.draw.rect(window,(135,0,178),r,0)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    pygame.display.flip()
 
 
 
