eb29bbbba378bd93cec9647199f1d4e94788b2c4
LubYT <lubister.vip@mail.ru>
2023-08-29 15:57:12 +0300

    2

--- Plugins/main.py
+++ Plugins/main.py
@@ -3,6 +3,8 @@ pygame.init()
 
 window = pygame.display.set_mode((pygame.display.Info().current_w,pygame.display.Info().current_h))
 
+#ПЕНИС
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
