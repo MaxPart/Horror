63388580957e1f8caa86ec07bf84183d86106268
MaxPart <mdolya@internet.ru>
2023-08-29 17:16:39 +0300

    1

--- Plugins/main.py
+++ Plugins/main.py
@@ -7,14 +7,18 @@ width_base=pygame.display.Info().current_w
 height_base=pygame.display.Info().current_h
 
 window = pygame.display.set_mode((width_base,height_base))
-window.fill((105,0,158))
+window.fill((32,0,56))
 
 r=pygame.Rect(100,100,int(width_base)-200,int(height_base)-200)
-pygame.draw.rect(window,(135,0,178),r,0)
+pygame.draw.rect(window,(50,0,87),r,0)
 
-font = pygame.font.SysFont('Bahnschrift', 80, bold=True)
-text = font.render(str(f"MaxPart's game"), True, 'green')
-window.blit(text,(int(width_base//2-350),int(height_base//2-200)))
+font = pygame.font.SysFont('comic_sans', 100, bold=True)
+text = font.render(str(f"HorrorTubbies"), True, 'green')
+window.blit(text,(int(width_base//2-400),int(height_base//2-300)))
+
+font = pygame.font.SysFont('comic_sans', 40, bold=True)
+text = font.render(str(f" Vers:0.1"), True, 'yellow')
+window.blit(text,(int(width_base//350-2),int(height_base//200-2)))
 
 while True:
     for event in pygame.event.get():
