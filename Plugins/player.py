9c70282c8e93ebad5ef7aeec9a3c28d39f94b751
LubYT <100532914+LubYT@users.noreply.github.com>
2023-09-01 09:47:24 +0300

    Update player.py
    доделай пж

--- Plugins/player.py
+++ Plugins/player.py
@@ -2,4 +2,5 @@ import pygame,pathlib
 class Player:
     def __init__(self):
         self.health=10
+        #self.rect=pygame.Rect() 
         self.image=pygame.image.load(pathlib.Path('..','Images','Chronos.png'))
